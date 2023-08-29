723d8f75b3865e69afb95bc9de2b2673ed672702
NickABN <115517667+NicolasAlejandroBasilioNava@users.noreply.github.com>
2023-08-28 23:11:25 -0600

    Tarea 4, Ejercicio de pantalla de Login

--- AwesomeProject/src/components/Login/LoginContent.jsx
+++ AwesomeProject/src/components/Login/LoginContent.jsx
@@ -1,10 +1,62 @@
-import { View, Text } from "react-native";
+import { StyleSheet, View, Text, TextInput, TouchableOpacity } from "react-native";
 
 export function LoginContent(){
     return(
-        <View>
-            <Text> LoginContent </Text>
+        <View style= {styles.container}>
+            <Text style={styles.label}> Email </Text>
+            <TextInput style={styles.input} placeholder="ejemplo@gmail.com"></TextInput>
+
+            <Text style={styles.label}> Password </Text>
+            <TextInput style={styles.input} placeholder="....." secureTextEntry={true}></TextInput>
+
+            <Text> Forgot Password?</Text>
+
+            <TouchableOpacity style={styles.touchableButton}>
+                <Text style= {styles.touchableText} > Login </Text>
+            </TouchableOpacity>
         </View>
     )
 
 }
+
+const styles = StyleSheet.create({
+    container: {
+      justifyContent: "flex-start",
+      alignItems: "flex-start",
+      paddingHorizontal: 20,
+      paddingVertical: 10,
+    },
+
+    label: {
+      marginBottom: 5,
+      color: 'gray',
+    },
+
+    input: {
+      width: 650,
+      height: 40,
+      borderWidth: 1,
+      borderColor: 'gray',
+      borderRadius: 15,
+      paddingHorizontal: 10,
+      marginVertical: 10,
+      marginTop: 5,
+      marginBottom: 10,
+    },
+
+    touchableButton: {
+        backgroundColor: '#c0e863',
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderRadius: 8,
+        elevation: 3,
+        alignSelf: 'center',
+        width: 600,
+        alignItems: 'center'
+      },
+
+      touchableText: {
+        color: 'black',
+        fontSize: 16,
+      },
+  })
--- AwesomeProject/src/components/Login/LoginHeader.jsx
+++ AwesomeProject/src/components/Login/LoginHeader.jsx
@@ -1,9 +1,40 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet} from "react-native";
 
 export function LoginHeader(){
+
+    const pricipalText = {
+
+    }
+
     return(
-        <View>
-            <Text> LoginHeader </Text>
+        <View style={styles.headerContainer}>
+            <Text style= {styles.tittleText} > Sign in to your Account </Text>
+            <Text style= {styles.secondaryText}> Sign in to your Account </Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    headerContainer:{
+        backgroundColor: '#152534',
+        //alignSelf: 'start',
+        justifyContent:'left',
+        height: 250,
+    },
+
+    tittleText:{
+        fontSize: 35,
+        color: 'white',
+        paddingTop: 150,
+        paddingHorizontal: 50
+        
+    },
+
+    secondaryText: {
+        fontSize: 15,
+        color: 'white',
+        paddingTop: 5,
+        paddingHorizontal: 60
+    }
+
+})
