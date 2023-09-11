f346e0be7a869e31bcce148d49be0b60cf18868f
NickABN <115517667+NicolasAlejandroBasilioNava@users.noreply.github.com>
2023-09-11 08:57:54 -0600

    Practica 9

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,4 +1,5 @@
-import { StyleSheet, Text, View, Image, Dimensions, TextInput, ScrollView, FlatList } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Image, Dimensions, TextInput, ScrollView, FlatList, TouchableOpacity } from 'react-native';
 import Constats from 'expo-constants'
 import { THEME } from './src/theme/colors';
 import WelcomeBackScreen from './src/screens/WelcomeBackScreen';
@@ -6,9 +7,28 @@ import CreateAccountScreen from './src/screens/CreateAccountScreen';
 import WelcomeScreen from './src/screens/WelcomeScreen';
 
 export default function App() {
+  const [state, setState] = useState(1)
+  const [inputValue, setIputValue] = useState()
+  function sumValue(){
+    setState(state+1)
+  }
+  //JSON.stringify(false)
+
+  console.log(state, '**STATE 0**')
+  console.log(setState, '**STATE 1**')
   return (
     <View style={styles.container}>
-      <WelcomeScreen />
+      <TextInput value= {inputValue}
+        onChangeText={(value) => setIputValue(value)}
+        style={{borderWidth: 1}}
+      />
+      <Text>{JSON.stringify(inputValue)}</Text>
+      
+      <Text>{JSON.stringify(state)}</Text>
+      <TouchableOpacity onPress={sumValue} style={{borderWidth: 1,}}>
+        <Text>Change Value</Text>
+      </TouchableOpacity>
+      {/* <WelcomeScreen /> */}
       {/* <WelcomeBackScreen /> */}
       {/* <CreateAccountScreen /> */}
     </View>
